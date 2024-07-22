86d59d5588ea382a8e6e8278bfe107e6a2868ff5
Aaryan Purohit <ap1922@ic.ac.uk>
2024-07-22 15:33:47 +0100

    CsvDatasetLoader - tests for CsvDatasetLoader class

---
+++ tests/test_datasets/CsvDatasetLoader_test.py
@@ -0,0 +1,10 @@
+from datasets.ExampleDatasets import get_example_dataset
+from datasets.custom_datasets.CsvDatasetLoader import CsvDatasetLoader
+
+
+def test_csv_dataset_loader():
+    ionosphere = get_example_dataset("ionosphere")
+    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/ionosphere/ionosphere.data"
+    column_names = [f"feature_{i}" for i in range(34)] + ["target"]
+    csv = CsvDatasetLoader(url, target_column="target", names=column_names)
+    assert csv.__eq__(ionosphere), "CSV Test: data does not match"

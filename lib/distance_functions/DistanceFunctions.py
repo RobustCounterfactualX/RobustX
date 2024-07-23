a22b4ee44bf2c9fabd262387736ebcbe511a64ea
Aaryan Purohit <ap1922@ic.ac.uk>
2024-07-23 17:36:58 +0100

    DistanceFunctions (creation) - the common distance functions can be shared across files so they are now extracted out to own module

---
+++ lib/distance_functions/DistanceFunctions.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+
+def euclidean(x: pd.DataFrame, c: pd.DataFrame):
+    return np.sqrt(np.sum((x.values - c.values) ** 2))
+
+
+def l1(x: pd.DataFrame, c: pd.DataFrame):
+    return np.sum(np.abs(x.values - c.values))

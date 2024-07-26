811b082f25fb5b9761d4ea9511776c99c5522304
Aaryan Purohit <ap1922@ic.ac.uk>
2024-07-26 15:12:38 +0100

    SimpleNNModel - Calling super constructor instead of directly assigning model

--- models/pytorch_models/SimpleNNModel.py
+++ models/pytorch_models/SimpleNNModel.py
@@ -8,12 +8,12 @@ from models.BaseModel import BaseModel
 
 class SimpleNNModel(BaseModel):
     def __init__(self, input_dim, hidden_dim, output_dim):
-        self.model = nn.Sequential(
+        super().__init__(nn.Sequential(
             nn.Linear(input_dim, hidden_dim),
             nn.ReLU(),
             nn.Linear(hidden_dim, output_dim),
             nn.Sigmoid()
-        )
+        ))
         self.criterion = nn.BCELoss()
         self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
 

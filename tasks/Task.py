96175a4e097390e69fa2e31fc82c2eda48d03415
Aaryan Purohit <ap1922@ic.ac.uk>
2024-08-28 01:14:48 +0100

    Task now stores a model again

--- tasks/Task.py
+++ tasks/Task.py
@@ -1,12 +1,14 @@
 from abc import ABC, abstractmethod
 import pandas as pd
 from datasets.DatasetLoader import DatasetLoader
+from models.BaseModel import BaseModel
 
 
 class Task(ABC):
 
-    def __init__(self, training_data: DatasetLoader):
+    def __init__(self, model: BaseModel, training_data: DatasetLoader):
         self._training_data = training_data
+        self.__model = model
 
     @abstractmethod
     def train(self):
@@ -18,3 +20,7 @@ class Task(ABC):
     @property
     def training_data(self):
         return self._training_data
+
+    @property
+    def model(self):
+        return self.__model

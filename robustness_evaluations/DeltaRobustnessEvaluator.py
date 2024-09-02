cdd917f4862efc334f9dc539462b50b9b3bda85d
Aaryan Purohit <ap1922@ic.ac.uk>
2024-09-02 20:47:36 +0100

    Changed DeltaRobustnessEvaluator.py - removed redundant TODOs and imports

--- robustness_evaluations/DeltaRobustnessEvaluator.py
+++ robustness_evaluations/DeltaRobustnessEvaluator.py
@@ -1,16 +1,10 @@
-import pandas as pd
 from gurobipy import Model, GRB
-from gurobipy.gurobipy import quicksum
 
-from intabs.IntervalAbstractionPyTorch import IntervalAbstractionPytorch
 from robustness_evaluations.ModelChangesRobustnessEvaluator import ModelChangesRobustnessEvaluator
 from tasks.Task import Task
 from lib.OptSolver import OptSolver
 
 
-# TODO: Validity and distance evaluators
-# TODO: Naive gradient method TODO: Make a Robustness Evaluator copy too which evaluates a group of points,
-#  a collection of these DeltaRobustnessEvaluators? Instantiate the point evaluator for each point.
 class DeltaRobustnessEvaluator(ModelChangesRobustnessEvaluator):
 
     def __init__(self, ct: Task):
@@ -19,7 +13,6 @@ class DeltaRobustnessEvaluator(ModelChangesRobustnessEvaluator):
 
     def evaluate(self, instance, desired_output=1, delta=0.5, bias_delta=0, M=1000000000, epsilon=0.0001):
 
-        # TODO: Ask if there's a better way to do this
         self.opt.gurobiModel = Model()
 
         self.opt.evaluate(instance, desired_output=desired_output, delta=delta, bias_delta=bias_delta, M=M,

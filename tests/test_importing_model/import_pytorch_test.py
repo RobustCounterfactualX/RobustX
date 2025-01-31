14519bf3c2f3bf574255070b8208347177608220
Lorenzo <le522@ic.ac.uk>
2025-01-31 14:43:41 +0000

    [le522] Refactor: removed duplicate test and cleaned up comments

--- tests/test_importing_model/import_pytorch_test.py
+++ tests/test_importing_model/import_pytorch_test.py
@@ -11,21 +11,22 @@ import pytest
 def trained_classification_task():
     model = SimpleNNModel(34, [8], 1)
     dl = get_example_dataset("ionosphere")
-
     ct = ClassificationTask(model, dl)
 
     dl.default_preprocess()
     ct.train()
+
     return ct
 
+
 def test_imported_pytorch_model_file_predict_single_same_as_original() -> None:
-    #Create Model
+    # Create Model
     ct = trained_classification_task()
 
-    #Save Model
+    # Save Model
     torch.save(ct.model.get_torch_model(), "./model.pt")
 
-    #Import Model
+    # Import Model
 
     trained_model = PytorchModel("./model.pt")
 
@@ -35,14 +36,15 @@ def test_imported_pytorch_model_file_predict_single_same_as_original() -> None:
 
     os.remove('./model.pt')
 
+
 def test_imported_pytorch_model_file_predict_all_same_as_original() -> None:
-    #Create Model
+    # Create Model
     ct = trained_classification_task()
 
-    #Save Model
+    # Save Model
     torch.save(ct.model.get_torch_model(), "./model.pt")
 
-    #Import Model
+    # Import Model
     trained_model = PytorchModel("./model.pt")
 
     predictions_1 = ct.model.predict(ct.training_data.data.drop("target", axis=1))
@@ -53,57 +55,33 @@ def test_imported_pytorch_model_file_predict_all_same_as_original() -> None:
 
 
 def test_imported_pytorch_model_from_instance_predict_single_same_as_original() -> None:
-    #Create Model
+    # Create Model
     ct = trained_classification_task()
 
     torch_model = ct.model.get_torch_model()
 
-    #Import Model
+    # Import Model
     trained_model = PytorchModel.from_model(torch_model)
 
     for _, instance in ct.training_data.data.iterrows():
         instance_x = instance.drop("target")
         assert ct.model.predict_single(instance_x) == trained_model.predict_single(instance_x)
 
-def test_imported_pytorch_model_from_instance_predict_all_same_as_original() -> None:
-    #Create Model
-    ct = trained_classification_task()
-
-    torch_model = ct.model.get_torch_model()
-
-    #Import Model
-    trained_model = PytorchModel.from_model(torch_model)
-
-    predictions_1 = ct.model.predict(ct.training_data.data.drop("target", axis=1))
-    predictions_2 = trained_model.predict(ct.training_data.data.drop("target", axis=1))
-    assert predictions_1.equals(predictions_2)
-
-
-def test_imported_pytorch_model_from_instance_predict_all_same_as_original() -> None:
-    #Create Model
-    ct = trained_classification_task()
-
-    torch_model = ct.model.get_torch_model()
-
-    #Import Model
-    trained_model = PytorchModel.from_model(torch_model)
-
-    predictions_1 = ct.model.predict(ct.training_data.data.drop("target", axis=1))
-    predictions_2 = trained_model.predict(ct.training_data.data.drop("target", axis=1))
-    assert predictions_1.equals(predictions_2)
 
 def test_throws_file_not_found_error() -> None:
     with pytest.raises(FileNotFoundError):
         trained_model = PytorchModel("./garbage.pt")
 
+
 def test_throws_wrong_file_type_error() -> None:
     with pytest.raises(TypeError):
         trained_model = PytorchModel("./test.txt")
 
+
 def test_throws_error_if_underlying_model_not_pytorch() -> None:
     with pytest.raises(InvalidPytorchModelError):
         ct = trained_classification_task()
-        #Save The SimpleNNModel rather than Torch Model
+        # Save The SimpleNNModel rather than Torch Model
         torch.save(ct.model, "./model.pt")
-        #Import Model
+        # Import Model
         trained_model = PytorchModel("./model.pt")

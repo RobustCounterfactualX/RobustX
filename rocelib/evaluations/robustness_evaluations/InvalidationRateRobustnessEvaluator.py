[le522] Fix: IR noise generation now does so with a covariance matrix

rocelib/evaluations/robustness_evaluations/InvalidationRateRobustnessEvaluator.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 from rocelib.evaluations.RecourseEvaluator import RecourseEvaluator
 from rocelib.tasks.Task import Task
@@ -52,27 +53,25 @@ class InvalidationRateRobustnessEvaluator(RecourseEvaluator):
         @param epsilon: A small constant used to ensure numerical stability.
         @return: A boolean indicating whether the model's prediction is robust given the desired output.
         """
+
+        # IMPORTANT: used dataset should be normalised so each feature is in range [0, 1]
         # instance is a single CE
 
         # use this to generate unique noise for every value in a df with more than one CEs
         # random_values = np.random.normal(loc=0, scale=5, size=df.shape)
         # df_new = df + random_values
 
-        mean = 0
-        stddev = 0.1
-        I = 1  # references in paper but i have no idea what it is
-        # print("BBBBBB")
-        # print(instance)
         instance = instance.drop(columns=["predicted", "Loss"], errors='ignore')
-        noise = pd.DataFrame([random.gauss(mean, stddev * I) for _ in range(len(instance.columns))]).transpose()
 
+        mean = np.zeros(len(instance.columns))
+        stddev = 0.1
+        cov_matrix = (stddev**2) * np.identity(len(instance.columns))
+
+        noise = np.random.multivariate_normal(mean, cov_matrix, size=len(instance))
         pred = self.task.model.predict(instance)
 
-        denormalised_noise = noise.values * (self.dataset_maxs - self.dataset_mins) + self.dataset_mins
+        denormalised_noise = noise * (self.dataset_maxs - self.dataset_mins) + self.dataset_mins
         pred_noisy = self.task.model.predict(instance + denormalised_noise)
-        print("--------")
-        print(pred_noisy)
-        print(pred)
 
         return pred == pred_noisy
 
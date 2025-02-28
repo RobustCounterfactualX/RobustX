8c5b70090dd5316b5e365a0fc5354cc7dffb2f89
Ayush Patel <62706040+ayushpatel4@users.noreply.github.com>
2025-02-28 14:22:30 +0000

    (ap2322) feat: example docs

--- tests/test_add_custom_recourse_method_evaluation_metric/add_custom_evaluation_metric_test.py
+++ tests/test_add_custom_recourse_method_evaluation_metric/add_custom_evaluation_metric_test.py
@@ -1,4 +1,5 @@
 from rocelib.evaluations.robustness_evaluations.Evaluator import Evaluator
+from rocelib.evaluations.robustness_evaluations.ModelChangesRobustnessEvaluator import ModelChangesRobustnessEvaluator
 from rocelib.tasks.TaskBuilder import TaskBuilder
 from rocelib.datasets.ExampleDatasets import get_example_dataset
 import pandas as pd
@@ -7,6 +8,10 @@ import pandas as pd
 class NewEvaluator(Evaluator):
     def evaluate(self, recourse_method, **kwargs):
         return -1
+
+class NewMCEvaluator(ModelChangesRobustnessEvaluator):
+    def evaluate_single_instance(self, instance, counterfactual, **kwargs):
+        return True
 
 
 def test_metric_gets_added_to_list_of_metrics():
@@ -28,3 +33,15 @@ def test_ce_evaluation_for_new_metric():
 
     expected_metric_result = -1
     assert evals["MCE"]["new_metric"] == expected_metric_result
+
+def test_ce_evaluation_for_new_robustness_metric():
+    dl = get_example_dataset("ionosphere")
+    # Not using testing models as we will be adding a recourse method to the task so could mess up other tests
+    ct = TaskBuilder().add_pytorch_model(34, [8], 1, dl).add_data(dl).build()
+
+    ct.add_evaluation_metric("new_mc_metric", NewMCEvaluator)
+    ces = ct.generate(["MCE"])
+    evals = ct.evaluate(["MCE"], ["new_mc_metric"])
+
+    #Check all eval results are True
+    assert all(evals["MCE"]["new_mc_metric"])
